527b9f587340843c64ee13edce4972320fd09c10
Cupid Valentine <westerngod@126.com>
2022-04-08 16:49:21 +0800

    config px to vw

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-
-
+import { Button } from 'antd-mobile'
+import './app.css'
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Button block color="primary" size="large">
+        Block Button
+      </Button>
+      <div></div>
+    </div>
+  )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import pxtovw from 'postcss-px-to-viewport'
 
+const loder_pxtovw = pxtovw({
+  // 设计稿是多宽就写多宽，这里的设计稿宽度为375
+  viewportWidth: 375,
+  // viewportUnit: 'vw'
+})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [loder_pxtovw]
     }
   }
 })
